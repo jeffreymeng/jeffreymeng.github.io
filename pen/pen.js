8f672f4b964cebb47fdc6e594fb6790b1129b367
jeffmeng <jkmexpress@yahoo.com>
2016-02-24 23:48:55 +0000

    t

--- pen/pen.js
+++ pen/pen.js
@@ -3,7 +3,9 @@
     'use strict';
     var pen = {};
     var defualt = {};
-    
+    var getParam = function() {
+        
+    }
     pen.info = {
         name: "penJS",
         version: "Private Alpha 1.0.0"
@@ -12,29 +14,30 @@
         var ctx = document.getElementById(id).getContext("2d");
         ctx.defualt = {};
         
-        ctx.rectangle = function(x, y, width, height, color) {
-            if (!color && width) {
-                color = this.defualt.rectangle.color || this.defualt.color || pen.defualt.color || "black";
+        ctx.rectangle = function(x, y, width, height, color, params) {
+            if (!color) {
+                color = this.defualt.rectangle.color || this.defualt.color || pen.defualt().color || "black";
             }
-            if (!height) {
-                height = this.defualt.rectangle.height || this.defualt.height || pen.getDefualt("height");
-            }
-            if (!width) {
-                width = this.defualt.rectangle.width || this.defualt.width || pen.getDefualt("width");
-                color = color || this.defualt.rectangle.color || this.defualt.color || pen.getDefualt("color") || "black";
+            if (arguments.length === 1) {
+                params = x;
             }
             
             this.fillStyle = color;
             this.fillRect(x,y,width,height);
 
         };
+        
         ctx.circle = function(startx, starty, endx, endy) {
             this.moveTo(startx,starty);
             this.lineTo(endx,endy);
             this.stroke();
+            
         };
         return ctx;
     };
+    pen.defualt = function() {
+        return defualt;
+    }
     window.pen = pen;
 
 })(window);
