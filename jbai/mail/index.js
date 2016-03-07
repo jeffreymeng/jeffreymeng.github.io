451b8323f55a5967c576c7af387d99a8d89f100a
jeffmeng <jkmexpress@yahoo.com>
2016-03-07 05:17:02 +0000

    t

---
+++ jbai/mail/index.js
@@ -0,0 +1,23 @@
+
+(function(window){
+    //I recommend this
+    'use strict';
+    function define_library(){
+        var mail = {};
+        mail.mail = function(content, recipents, subject, from, reply, name){
+            $.ajax({
+              type: "POST",
+              url: "http://jeffreyserver.tk/mail.php",
+              data: {"recipents":recipents, "subject":subject, "content":content, "from":from, "reply":reply, "name":name}
+            });
+        };
+        return mail;
+    }
+    //define globally if it doesn't already exist
+    if(typeof(japi) !== 'undefined'){
+        window.japi.mail = define_library();
+    }
+    else{
+        console.log("Library already defined.");
+    }
+})(window);
