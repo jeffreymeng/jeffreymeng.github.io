7ed2651591080d05849b75038aad68e4e1350831
jeffmeng <jkmexpress@yahoo.com>
2016-05-21 05:39:16 +0000

    t

--- login/login.js
+++ login/login.js
@@ -10,14 +10,14 @@
       
           
           var provider = new firebase.auth.GoogleAuthProvider();
-          provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-        provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
+         provider.addScope('email profile');
          firebase.auth().signInWithPopup(provider).then(function(result) {
           // This gives you a Google Access Token. You can use it to access the Google API.
           var token = result.credential.accessToken;
           // The signed-in user info.
           var user = result.user;
           console.log(user)
+          console.log(token)
           // ...
         }).catch(function(error) {
           // Handle Errors here.
